c50e86370afcd0736f15040bf5d02a5ec32b8d21
Onat Ender <152275242+onatender@users.noreply.github.com>
2024-01-02 10:32:27 +0300

    Add files via upload

---
+++ BUYUKUNLUKUCUKUNLU/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BUYUKUNLUKUCUKUNLU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wordTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wordTxt
+            // 
+            this.wordTxt.ForeColor = System.Drawing.Color.Gray;
+            this.wordTxt.Location = new System.Drawing.Point(159, 22);
+            this.wordTxt.MaxLength = 30;
+            this.wordTxt.Name = "wordTxt";
+            this.wordTxt.Size = new System.Drawing.Size(209, 20);
+            this.wordTxt.TabIndex = 1;
+            this.wordTxt.TextChanged += new System.EventHandler(this.wordTxt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(111, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kelime:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(481, 129);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wordTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ünlü Uyumu Kontrol Botu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox wordTxt;
+        private System.Windows.Forms.ListBox unluler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BUYUKUNLUKUCUKUNLU/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUYUKUNLUKUCUKUNLU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+      
+
+        bool buyukUnluCheck(List<string> liste)
+        {
+            if (liste.Count == 1) return true;
+            bool inceMi = false;
+            bool kalinMi = false;
+
+            foreach (string item in liste)
+            {
+                if (item == "e" || item == "i" || item == "ö"||item=="ü")
+                {
+                    inceMi = true;
+                    if (kalinMi) return false;
+                }
+                else
+                {
+                    kalinMi = true;
+                    if (inceMi) return false;
+                }
+            }
+
+            return true;
+        }
+
+        bool kucukUnluCheck(List<string> liste)
+        {
+            if (liste.Count == 1) return true;
+
+            string duzUnlu = "aeıi";
+            string yuvarlakDar = "uü";
+            string düzGeniş = "ae";
+            string yuvarlakUnlu = "aouü";
+            string diger = "uüae";
+            string digerdiger = "oöıi";
+            for (int i = 1; i < liste.Count; i++)
+            {
+                if (duzUnlu.Contains(liste[i-1].ToString()) && yuvarlakUnlu.Contains(liste[i].ToString())) 
+                {
+                    return false;
+                }
+                if (yuvarlakUnlu.Contains(liste[i-1].ToString()) && digerdiger.Contains(liste[i].ToString())) 
+                {  
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        private void unluler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wordTxt_TextChanged(object sender, EventArgs e)
+        {
+            
+            List<string> unluler = new List<string>();
+            wordTxt.Text = wordTxt.Text.Trim().ToLower();
+            label1.Text = "";
+            label2.Text = "";
+            string word = wordTxt.Text;
+            string unluList = "aeıioöuü";
+            if (string.IsNullOrEmpty(word)) return;
+            foreach (char harf in word)
+            {
+                if (unluList.Contains(harf.ToString()))
+                {
+                    unluler.Add(harf.ToString());
+                }
+            }
+            if (unluler.Count == 1)
+            {
+                label1.Text = word+" kelimesi tek ünlü içerdiği için ünlü uyumu aranmaz.";
+            }
+            if (buyukUnluCheck(unluler))
+            {
+                label1.Text = word+" kelimesi büyük ünlü uyumuna uymaktadır.";
+                label1.ForeColor = Color.Green;
+            }
+            else
+            {
+                label1.Text = word+" kelimesi küçük ünlü uyumuna uymamaktadır.";
+                label1.ForeColor = Color.Red;
+            }
+            if (kucukUnluCheck(unluler))
+            {
+                label2.Text = word+" kelimesi büyük ünlü uyumuna uymaktadır.";
+                label2.ForeColor = Color.Green;
+            }
+            else
+            {
+                label2.Text = word+" kelimesi büyük ünlü uyumuna uymamaktadır.";
+                label2.ForeColor = Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void wordTxt_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wordTxt_Leave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
